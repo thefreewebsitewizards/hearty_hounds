c1e1cf9fb4fa06e0b8ae916529b02d37fc6db409
Bench <dylan@thefreewebsitewizards.com>
2025-09-05 18:20:32 +0800

    up

--- src/pages/Auth.tsx
+++ src/pages/Auth.tsx
@@ -108,6 +108,7 @@ const Auth: React.FC = () => {
                 </label>
                 <div className="mt-1 relative">
                   <div className="absolute inset-y-0 left-0 pl-3 flex items-center pointer-events-none">
+                    {/* @ts-ignore */}
                     <FiUser className="h-5 w-5 text-gray-400" />
                   </div>
                   <input
